cd88274304f8261e2940cd0e570e6cc8f941d4da
Ivan Ramirez <ramirezfranciscoivan@gmail.com>
2018-09-13 10:30:24 -0600

    Initial Commit

---
+++ PokemonSearchiOS21/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokemonSearchiOS21
+//
+//  Created by Ivan Ramirez on 9/13/18.
+//  Copyright © 2018 ramcomw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
